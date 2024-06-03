e0aafe020bd792790e1ed1dbd126c0ad3feac486
Romeu Riffatti <romeufftt@gmail.com>
2024-06-03 18:30:09 -0300

    criar banco

--- fakepintereskcerto/__init__.py
+++ fakepintereskcerto/__init__.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://banco_fakepinterest_9wc5_user:HhGy23563Q5VCNPsnf9qwfbLFzmMDuIa@dpg-cpf32pjtg9os73b6rdng-a.oregon-postgres.render.com/banco_fakepinterest_9wc5"
 app.config["SECRET_KEY"] = "354ith5b3bjvhsvgy8425tbnirug97842"
 app.config['UPLOAD_FOLDER'] = "static/fotos_posts"
 database = SQLAlchemy(app)
